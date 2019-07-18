a67c338e2dc7c71131af8c5e67017c052e093d0e
Ryan Wilson <xryanwilson@gmail.com>
2019-07-18 00:39:07 -0400

    integrated rachio api into data fetch/aggregation process

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const husqv = {
     api: require("./utils/husqvarna-api"),
     internal: require("./utils/husqvarna-internal")
 };
+const rachio = require("./utils/rachio-api");
 
 /**
  * Schedules the script to run every 15 minutes.
  */
 
-cron.schedule("*/15 * * * *", () => {
-    init();
-});
+// cron.schedule("*/15 * * * *", () => {
+//     init();
+// });
 
 /**
  * Executes API requests in parallel before writing fetched data to database.
@@ -23,21 +24,37 @@ cron.schedule("*/15 * * * *", () => {
 
 const init = async () => {
     try {
-        let records = [];
+        let mowers = [];
+        let sprinklers = [];
 
-        const { access_token: externalToken, user_id: userId } = await husqv.api.getToken();    // request access token
-        const { data: { id: internalToken }} = await husqv.internal.getToken();                 // and user information
-        const { data: externalMowers } = await husqv.api.getMowers(externalToken);              // fetch mower data from API
-        const internalMowers = await husqv.internal.getMowers(internalToken);                   // and internal endpoints
+        // fetch internal and external mower data from Husqvarna endpoints
 
-        // parse and aggregate external and internal mower data, as well as current and
-        // forecasted weather conditions into an array.
+        const { access_token: husqvToken, user_id: husqvUserId } = await husqv.api.getToken();
+        const { data: { id: husqvId }} = await husqv.internal.getToken();
+        const { data: mowersExternal } = await husqv.api.getMowers(husqvToken);
+        const mowersInternal = await husqv.internal.getMowers(husqvId);
 
-        records = await aggregateExternalData(userId, externalMowers);
-        records = await aggregateInternalData(internalMowers, records, internalToken);
-        records = await aggregateWeatherConditions(records);
+        // parse and aggregate internal and external mower data, as well
+        // as current and forecasted weather conditions into an array
+
+        mowers = await aggregateExternalMowerData(husqvUserId, mowersExternal);
+        mowers = await aggregateInternalMowerData(mowersInternal, mowers, husqvId);
+        mowers = await aggregateMowerWeatherConditions(mowers);
+
+        // request sprinkler data from Rachio endpoints
         
-        db.bulkInsert(records);     // write records to database
+        const { id: rachioToken } = await rachio.getToken();        
+        const { id: rachioUserId, devices } = await rachio.getDevices(rachioToken);
+
+        // parse and aggregate sprinkler data, as well as current and forecasted weather conditions into an array
+
+        sprinklers = await aggregateSprinklerData(rachioUserId, devices);
+        sprinklers = await aggregateSprinklerWeatherConditions(sprinklers);
+
+        // insert records into database
+
+        db.insertMowers(mowers);
+        db.insertSprinklers(sprinkers);
     } catch (e) {
         logger.log(e.message, e);
     }
@@ -46,11 +63,12 @@ const init = async () => {
 /**
  * Parses and populates a list of external mowers into the record list.
  * 
+ * @param {string} userId the Husqvarna user identifier
  * @param {Array<Object>} mowers the list of mowers
  * @returns a list of records after adding external mower data
  */
 
-const aggregateExternalData = (userId, mowers) => {
+const aggregateExternalMowerData = (userId, mowers) => {
     let records = [];
 
     mowers.forEach((mower) => {
@@ -88,7 +106,7 @@ const aggregateExternalData = (userId, mowers) => {
  * @returns a list of records after adding internal mower data
  */
 
-const aggregateInternalData = async (mowers, records, token) => {
+const aggregateInternalMowerData = async (mowers, records, token) => {
     logger.log("Fetching mower status from Husqvarna internal endpoint.");
     logger.log("Fetching geofence data from Husqvarna internal endpoint.");
     logger.log("Fetching settings data from Husqvarna internal endpoint.");
@@ -132,13 +150,13 @@ const aggregateInternalData = async (mowers, records, token) => {
 };
 
 /**
- * Parses and aggregates current and forecasted weather conditions into the record list.
+ * Parses and aggregates current and forecasted weather conditions for mower locations into the record list.
  * 
  * @param {Array<Object>} records the list of existing records
  * @returns a list of records after adding current and forecasted weather conditions
  */
 
-const aggregateWeatherConditions = async (records) => {
+const aggregateMowerWeatherConditions = async (records) => {
     logger.log("Fetching current and forecasted weather conditions from Dark Sky API.");
 
     for (let i = 0; i < records.length; i++) {
@@ -208,3 +226,133 @@ const aggregateWeatherConditions = async (records) => {
 
     return records;
 };
+
+/**
+ * Parses and populates a list of sprinklers into the record list.
+ * 
+ * @param {string} userId the unique Rachio user identifier
+ * @param {Array<Object>} sprinklers the list of sprinklers
+ * @returns a list of records after adding sprinkler data
+ */
+
+const aggregateSprinklerData = async (userId, sprinklers) => {
+    let records = [];
+
+    sprinklers.forEach((sprinkler) => {
+        records.push({
+            manufacturer: "rachio",
+            deviceId: sprinkler.id,
+            deviceName: sprinkler.name,
+            deviceModel: sprinkler.model,
+            serialNo: sprinkler.serialNumber,
+            macAddress: sprinkler.macAddress,
+            latitude: sprinkler.latitude,
+            longitude: sprinkler.longitude,
+            zones: sprinkler.zones.filter((zone) => {
+                zone.zoneId = zone.id;
+                zone.zoneName = zone.name;
+                zone.sqft = zone.yardAreaSquareFeet;
+                zone.nozzleName = zone.customNozzle.name;
+                zone.inchesPerHour = zone.customNozzle.inchesPerHour;
+                zone.soilName = zone.customSoil.name;
+                zone.slopeName = zone.customSlope.name;
+                zone.slopeSortOrder = zone.customSlope.sortOrder;
+                zone.cropName = zone.customCrop.name;
+                zone.cropCoefficient = zone.customCrop.coefficient;
+                zone.shadeName = zone.customShade.name;
+    
+                delete zone.id;
+                delete zone.name;
+                delete zone.yardAreaSquareFeet;
+                delete zone.customNozzle;
+                delete zone.customSoil;
+                delete zone.customSlope;
+                delete zone.customCrop;
+                delete zone.customShade;
+                delete zone.wateringAdjustmentRuntimes;
+    
+                return true;
+            }).sort((a, b) => {
+                return a.zoneNumber - b.zoneNumber;
+            }),
+            scheduleRules: sprinkler.scheduleRules.filter((scheduleRule) => {
+                scheduleRule.scheduleId = scheduleRule.id;
+                scheduleRule.applicableZones = scheduleRule.zones.sort((a, b) => {
+                    return a.sortOrder - b.sortOrder;
+                });
+
+                delete scheduleRule.id;
+                delete scheduleRule.zones;
+                delete scheduleRule.startDay;
+                delete scheduleRule.startMonth;
+                delete scheduleRule.startYear;
+
+                return true;
+            }),
+            poweredOn: sprinkler.on,
+            scheduleModeType: sprinkler.scheduleModeType,
+            homekitCompatible: sprinkler.homekitCompatible,
+            status: sprinkler.status,
+            isDeleted: sprinkler.deleted,
+            userId: userId
+        });
+    });
+
+    return records;
+};
+
+/**
+ * Parses and aggregates current and forecasted weather conditions for sprinkler locations into the record list.
+ * 
+ * @param {Array<Object>} records the list of existing records
+ * @returns a list of records after adding current and forecasted weather conditions
+ */
+
+const aggregateSprinklerWeatherConditions = async (records) => {
+    logger.log("Fetching current and forecasted weather conditions from Rachio API.");
+
+    for (let i = 0; i < records.length; i++) {
+        const record = records[i];
+        const { current, forecast } = await rachio.getWeatherConditions(record.deviceId);
+
+        record.weather = [{
+            forecastType: "currently",
+            forecastDay: -1,
+            summary: current.weatherSummary,
+            precipIntensity: current.precipIntensity,
+            precipChance: current.precipProbability,
+            temp: current.currentTemperature,
+            tempHigh: undefined,
+            tempLow: undefined,
+            dewPoint: current.dewPoint,
+            humidity: current.humidity,
+            windSpeed: current.windSpeed,
+            cloudCover: current.cloudCover
+        }];
+
+        for (let j = 0; j < forecast.length; j++) {
+            const daily = forecast[j];
+
+            record.weather.push({
+                forecastType: "daily",
+                forecastDay: j,
+                summary: daily.weatherSummary,
+                precipIntensity: daily.precipIntensity,
+                precipChance: daily.precipProbability,
+                temp: undefined,
+                tempHigh: daily.temperatureMax,
+                tempLow: daily.temperatureMin,
+                dewPoint: daily.dewPoint,
+                humidity: daily.humidity,
+                windSpeed: daily.windSpeed,
+                cloudCover: daily.cloudCover
+            });
+        }
+
+        records.splice(i, 1, record);
+    }
+
+    return records;
+};
+
+init();
